10ee46b3dc29aad8b5b8161586a620fb494579a2
deepakTricon <deepakg@triconinfotech.com>
2017-09-05 13:34:44 +0530

    intial commit

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'prem', 
+			firstName: 'Prem', 
+			lastName: 'Singh'
+		},	
+		{
+			id: 'gaurav', 
+			firstName: 'Gaurav', 
+			lastName: 'Raj'
+		},	
+		{
+			id: 'sachin', 
+			firstName: 'Sachin', 
+			lastName: 'Shukla'
+		}
+	]
+};
